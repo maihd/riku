db406d6f777076b67a17b90756ec9211de32170d
MaiHD <maihd.dev@gmail.com>
2019-02-16 21:19:57 +0700

    Simple interface for Array

--- include/riku/array.h
+++ include/riku/array.h
@@ -5,4 +5,46 @@ struct Array
 {
 public:
     T* buffer;
+
+public:
+    inline Array()
+        : buffer(nullptr)
+    {
+    }
+
+    inline ~Array()
+    {
+    }
+
+public:
+    inline int size()
+    {
+        return buffer ? ((int*)buffer - 2)[0] : 0;
+    }
+
+    inline int count()
+    {
+        return buffer ? ((int*)buffer - 2)[1] : 0;
+    }
+
+public:
+    inline T& operator[](int index)
+    {
+        return buffer[index];
+    }
+
+    inline const T& operator[](int index) const
+    {
+        return buffer[index];
+    }
 };
+
+namespace array 
+{
+    template <typename T>
+    bool push(Array<T>& array, const T& value)
+    {
+        // Not implement yet
+        return false;   
+    }
+}
