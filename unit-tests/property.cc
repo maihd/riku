61976bb2a9b0158a0225e7481cf4326adf58bdf0
MaiHD <maihd.dev@gmail.com>
2019-02-16 23:51:33 +0700

    property syntax completely work with clang and Visual C++

--- unit-tests/property.cc
+++ unit-tests/property.cc
@@ -2,6 +2,8 @@
 
 #include "../include/riku/property.h"
 
+#define test_assert(a, b) printf(#a " = %d - " #b " = %d\n", a, b)
+
 struct PropertyTest
 {
 public:
@@ -10,53 +12,53 @@ public:
 public:
     int get_value()
     {
-        return _value;
+        return real_value;
     }
 
     void set_value(int v)
     {
-        _value = v;
+        real_value = v;
     }
 
-private: // Use private only for this feature, donot use in real-life code
-    int _value;
+public:
+    int real_value;
 };
 
 int main()
 {
     PropertyTest test;
     test.value = 0;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 1;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 1;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 2;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 3;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 4;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 5;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 6;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 7;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 8;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     test.value = 9;
-    printf("test.value = %d\n", test.value);
+    test_assert(test.value, test.real_value);
 
     return 0;
 }
