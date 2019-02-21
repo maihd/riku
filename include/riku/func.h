d7daa6027f2273cb9394ab3cc41e9de82bce9e63
MaiHD <maihd.dev@gmail.com>
2019-02-21 15:50:33 +0700

    Simple Func API

---
+++ include/riku/func.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "./define.h"
+#include "./property.h"
+
+template <typename TRet, typename ...TArgs>
+struct Func;
+
+template <typename TRet, typename ...TArgs>
+struct Func<TRet(TArgs...)>
+{
+public: // 
+    void* buffer;
+
+public: // Properties
+    property_readonly(get_type) int type;
+
+public: // Properties implement
+    inline int get_type() const 
+    {
+        return buffer ? ((int*)buffer)[0] : 0; 
+    }
+
+public: // Operators
+    inline TRet operator(TArgs... args) const
+    {
+        return TRet();
+    }
+};
--- include/riku/property.h
+++ include/riku/property.h
@@ -3,6 +3,6 @@
 #if __GNUC__ && !__clang__
 #error "riku::property: No GCC support, sorry man."
 #else
-#define property(type, getter, setter)  __declspec(property(get=getter, put=setter)) type
-#define property_readonly(type, getter) __declspec(property(get=getter)) type
+#define property(getter, setter)  __declspec(property(get=getter, put=setter))
+#define property_readonly(getter) __declspec(property(get=getter))
 #endif
