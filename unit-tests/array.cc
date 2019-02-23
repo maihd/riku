56b27cd73c8f0e270897164f3f42937f42379955
MaiHD <maihd.dev@gmail.com>
2019-02-23 22:20:28 +0700

    More robust testing

--- unit-tests/array.cc
+++ unit-tests/array.cc
@@ -10,61 +10,45 @@ TEST_CASE("Test array operations", "[Array]")
 {
     Array<int> int_array, int_array1;
 
-    array::push(int_array, 0);
-    array::push(int_array, 1);
-    array::push(int_array, 2);
-    array::push(int_array, 3);
-    array::push(int_array, 4);
-    array::push(int_array, 5);
-    array::push(int_array, 6);
-    array::push(int_array, 7);
-    array::push(int_array, 8);
-    array::push(int_array, 9);
+    for (int i = 0, n = 10; i < n; i++)
+    {
+        array::push(int_array, i);
+    }
+    REQUIRE(int_array.length == 10);
 
     printf("int_array after pushing...\n");
     for (int i = 0, n = int_array.length; i < n; i++)
     {
+        REQUIRE(int_array[i] == i);
         printf("\tint_array[%d] = %d\n", i, int_array[i]);
     }
+    
+    printf("Clearing int_array with array::pop()\n");
+    for (int i = 0, n = int_array.length; i < n; i++)
+    {
+        array::pop(int_array);
+    }
+    REQUIRE(int_array.length == 0);
 
-    array::pop(int_array);
-    array::pop(int_array);
-    array::pop(int_array);
-    array::pop(int_array);
-    array::pop(int_array);
-    array::pop(int_array);
-    array::pop(int_array);
-    array::pop(int_array);
-    array::pop(int_array);
-    array::pop(int_array);
-
-    array::unshift(int_array, 0);
-    array::unshift(int_array, 1);
-    array::unshift(int_array, 2);
-    array::unshift(int_array, 3);
-    array::unshift(int_array, 4);
-    array::unshift(int_array, 5);
-    array::unshift(int_array, 6);
-    array::unshift(int_array, 7);
-    array::unshift(int_array, 8);
-    array::unshift(int_array, 9);
+    for (int i = 0, n = 10; i < n; i++)
+    {
+        array::unshift(int_array, i);
+    }
+    REQUIRE(int_array.length == 10);
 
     printf("int_array after unshifting...\n");
     for (int i = 0, n = int_array.length; i < n; i++)
     {
+        REQUIRE(int_array[i] == n - i - 1);
         printf("\tint_array[%d] = %d\n", i, int_array[i]);
     }
-
-    array::shift(int_array);
-    array::shift(int_array);
-    array::shift(int_array);
-    array::shift(int_array);
-    array::shift(int_array);
-    array::shift(int_array);
-    array::shift(int_array);
-    array::shift(int_array);
-    array::shift(int_array);
-    array::shift(int_array);
+    
+    printf("Clearing int_array with array::shift()\n");
+    for (int i = 0, n = int_array.length; i < n; i++)
+    {
+        array::shift(int_array);
+    }
+    REQUIRE(int_array.length == 0);
 
     array::push(int_array, 0);
     array::push(int_array, 1);
@@ -76,6 +60,7 @@ TEST_CASE("Test array operations", "[Array]")
     array::push(int_array, 7);
     array::push(int_array, 8);
     array::push(int_array, 9);
+    REQUIRE(int_array.length == 10);
 
     array::push(int_array1, 0);
     array::push(int_array1, 1);
@@ -87,31 +72,21 @@ TEST_CASE("Test array operations", "[Array]")
     array::push(int_array1, 7);
     array::push(int_array1, 8);
     array::push(int_array1, 9);
+    REQUIRE(int_array.length == 10);
 
-    array::index_of(int_array, 0);
-    array::index_of(int_array, 1);
-    array::index_of(int_array, 2);
-    array::index_of(int_array, 3);
-    array::index_of(int_array, 4);
-    array::index_of(int_array, 5);
-    array::index_of(int_array, 6);
-    array::index_of(int_array, 6);
-    array::index_of(int_array, 7);
-    array::index_of(int_array, 8);
-    array::index_of(int_array, 9);
-    array::last_index_of(int_array1, 0);
-    array::last_index_of(int_array1, 1);
-    array::last_index_of(int_array1, 2);
-    array::last_index_of(int_array1, 3);
-    array::last_index_of(int_array1, 4);
-    array::last_index_of(int_array1, 5);
-    array::last_index_of(int_array1, 6);
-    array::last_index_of(int_array1, 6);
-    array::last_index_of(int_array1, 7);
-    array::last_index_of(int_array1, 8);
-    array::last_index_of(int_array1, 9);
+    for (int i = 0, n = int_array.length; i < n; i++)
+    {
+        REQUIRE(array::index_of(int_array, int_array[i]) == i);
+    }
 
+    for (int i = 0, n = int_array1.length; i < n; i++)
+    {
+        REQUIRE(array::last_index_of(int_array1, int_array1[i]) == i);
+    }
+
+    int new_length = int_array.length + int_array1.length;
     array::concat(int_array, int_array1);
+    REQUIRE(int_array.length == new_length);
 
     printf("int_array after concat...\n");
     for (int i = 0, n = int_array.length; i < n; i++)
