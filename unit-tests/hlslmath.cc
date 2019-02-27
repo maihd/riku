73e8658fcf3f968cce1f87ffb9e8858a644d9e45
MaiHD <maihd.dev@gmail.com>
2019-02-27 15:57:42 +0700

    Math module

--- unit-tests/hlslmath.cc
+++
@@ -1,8 +0,0 @@
-#include <riku/hlslmath.h> // Just compile
-
-//#include <cmath>
-#include "./unit_test.h"
-
-TEST_CASE("HLSL Math", "[math]")
-{
-}
---
+++ unit-tests/hmath.cc
@@ -0,0 +1,12 @@
+#include <riku/math.h> // Just compile
+
+//#include <cmath>
+#include "./unit_test.h"
+
+TEST_CASE("HLSL Math", "[math]")
+{
+    auto a = fabsf(10);
+    auto b = fabsf(10.0f);
+
+    auto c = cos(10.0f);
+}
