2f8042a36ca8ca63ff6058a75de297de0ec2c51a
MaiHD <maihd.dev@gmail.com>
2019-03-04 23:36:42 +0700

    Path module's API

--- include/riku/path.h
+++ include/riku/path.h
@@ -4,5 +4,34 @@
 
 namespace path
 {
-    
+#if OS_WINDOWS
+    const char* delimiter = ";";
+#else
+    const char* delimiter = ":";
+#endif
+
+    usize basename(const char* path, char* buffer, usize length);
+    usize basename(const char* path, const char* ext, char* buffer, usize length);
+
+    const char* basename(const char* path);
+    const char* basename(const char* path, const char* ext);
+
+    usize       dirname(const char* path, char* buffer, usize length);
+    const char* dirname(const char* path);
+
+    usize       extname(const char* path, char* buffer, usize length);
+    const char* extname(const char* path);
+
+    bool        is_absolute(const char* path);
+    const char* get_absolute(const char* path);
+    usize       get_absolute(const char* path, char* buffer, usize length);
+
+    const char* join(const char* path, ...);
+    usize       join(char* buffer, usize length, const char* path, ...);
+
+    const char* relative(const char* from, const char* to);
+    usize       relative(const char* from, const char* to, char* buffer, usize length);
+
+    const char* relative(const char* path, ...);
+    usize       relative(char* buffer, usize length, const char* path, ...);
 }
