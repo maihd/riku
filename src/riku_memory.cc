7c6f9c971a032c7757798a1d6f9cf7caf7250f4d
MaiHD <maihd.dev@gmail.com>
2019-04-01 09:31:42 +0700

    Make function call of memory module more explicit

--- src/riku_memory.cc
+++ src/riku_memory.cc
@@ -10,58 +10,58 @@ namespace memory
 {
     void* alloc(int size)
     {
-        return alloc(size, 16);
+        return memory::alloc(size, 16);
     }
 
     void dealloc(void* ptr)
     {
         if (ptr)
         {
-            void* org_ptr = dealign(ptr);
-            free(org_ptr);
+            void* org_ptr = memory::dealign(ptr);
+            ::free(org_ptr);
         }
     }
 
     void* realloc(void* ptr, int size)
     {
-        return realloc(ptr, size, 16);
+        return memory::realloc(ptr, size, 16);
     }
 
     void* alloc(int size, int align)
     {
-        return memory::align(malloc((usize)size + align), align);
+        return memory::align(::malloc((usize)size + align), align);
     }
 
     void* realloc(void* ptr, int size, int align)
     {
         if (ptr)
         {
-            void* org_ptr = dealign(ptr);
+            void* org_ptr = memory::dealign(ptr);
             return memory::align(::realloc(org_ptr, (usize)(size + align)), align);
         }
         else
         {
-            return alloc(size, align);
+            return memory::alloc(size, align);
         }
     }
 
     void* init(void* dst, int val, int size)
     {
-        return memset(dst, val, (usize)size);
+        return ::memset(dst, val, (usize)size);
     }
 
     void* copy(void* dst, const void* src, int size)
     {
-        return memcpy(dst, src, (usize)size);
+        return ::memcpy(dst, src, (usize)size);
     }
 
     void* move(void* dst, const void* src, int size)
     {
-        return memmove(dst, src, (usize)size);
+        return ::memmove(dst, src, (usize)size);
     }
 
     int compare(const void* a, const void* b, int size)
     {
-        return memcmp(a, b, (usize)size);
+        return ::memcmp(a, b, (usize)size);
     }
 }
